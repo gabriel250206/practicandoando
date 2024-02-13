41f92d9c4e4134468836b8bb883cd3e3bf1a03a5
gabriel250206 <19050@portalcedi.edu.mx>
2024-02-13 12:29:03 -0600

    A

--- oli.cpp
+++ oli.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+
 using namespace std;
+
+class Persona
+{
+private:
+    int edad;
+
+public:
+    int nombre;
+
+    Persona(int ed)
+    {
+        if (ed >= 0)
+        {
+            edad = ed;
+        }
+        else
+        {
+            edad = 0;
+        }
+    }
+    int getEdad()
+    {
+        return edad;
+    }
+};
 
 int main()
 {
-
-    cout << "hello world" << endl;
+    int x;
+    cin >> x;
+    Persona yo(x);
+    cout << "hola " << yo.getEdad();
     return 0;
 }
